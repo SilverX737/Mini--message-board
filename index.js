6087b6e45529d2e0672a4df2e142e8abc3813fe4
Miran <miranabdelaziz@yahoo.com>
2024-11-17 16:15:15 -0500

    - added views to display messages - added post request to handle new mesages

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
+const path = require("node:path");
+
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+
+
+app.get("/", (req, res) => {
+    res.render("index", { messages: messages });
+  });
+app.get("/new",(req,res) => res.render("form"));
+
+
+app.post("/new", (req, res) => {
+    const { text, user } = req.body;
+    if (text && user) {
+      messages.push({ text, user, added: new Date() }); // Add the new message
+    }
+    res.redirect("/"); // Redirect back to the homepage
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
